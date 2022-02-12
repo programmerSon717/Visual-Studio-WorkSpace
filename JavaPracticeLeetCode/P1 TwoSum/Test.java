06dfd77ebe4ce1d76ffee5dd1cc45e69b709c505
Woojin Son <programson717@gmail.com>
2022-02-12 10:19:20 -0500

    Fail

--- JavaPracticeLeetCode/P1 TwoSum/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test extends TwoSum{
-    public static void main(String[] args){
-        int[] nums={6,4,3,8,7,5,2};
-        TwoSum ts=new TwoSum();
-        int[] result=ts.twoSum(nums,5);
-        System.out.println(result[0]+", "+ result[1]);
-    }
-}
